7354539796ccaf5bf435a59ee1a4ca1263a94873
nahid-hstu57 <hmdnahid321@gmail.com>
2025-03-02 23:04:08 +0600

    almost completed

--- js/java.js
+++ js/java.js
@@ -1,11 +1,22 @@
 document.getElementById("main-btn").addEventListener('click',function(){
     window.location.href = "/index.html";
 })
-document.getElementById("go-to-main").addEventListener('mouseout', function () {
-    window.location.href = "main.html";
-    console.log("clicked"); 
-})
-document.getElementById("btn-task-1").addEventListener('click',function(){
-    alert("task completed")
-    console.log("akdfj");
-})
+let sum=0;
+task_completed("btn-task-1");
+function task_completed(name){
+    document.getElementById("name").addEventListener('click',function(){
+        this.style.backgroundColor="gray";
+        alert("task completed");
+        sum++;
+        if(sum==6){
+            alert("");
+        }
+        const container = document.getElementById("comment-section")
+        const p=document.createElement("h3");
+        p.innerText ='
+        "task completed"
+        '
+        container.appendChild(p)
+        
+    })
+}
