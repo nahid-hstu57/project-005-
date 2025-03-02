489622f9a6e75be989e89ddfdae6a54e0d3f8635
nahid-hstu57 <hmdnahid321@gmail.com>
2025-03-02 23:49:06 +0600

    last commit

--- js/java.js
+++ js/java.js
@@ -1,22 +1,2 @@
 document.getElementById("main-btn").addEventListener('click',function(){
     window.location.href = "/index.html";
-})
-let sum=0;
-task_completed("btn-task-1");
-function task_completed(name){
-    document.getElementById("name").addEventListener('click',function(){
-        this.style.backgroundColor="gray";
-        alert("task completed");
-        sum++;
-        if(sum==6){
-            alert("");
-        }
-        const container = document.getElementById("comment-section")
-        const p=document.createElement("h3");
-        p.innerText ='
-        "task completed"
-        '
-        container.appendChild(p)
-        
-    })
-}
